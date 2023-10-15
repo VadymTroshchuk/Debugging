3496587c29e88765a039e4ffe5e46694abd129aa
Vadym Troshchuk <vadimtrosuk01@gmail.com>
2023-10-15 20:36:25 +0300

    add debugging example

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.debugging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 
 private const val TAG = "MainActivity"
@@ -12,10 +13,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "this is where the app crashed before")
+        val helloTextView: TextView = findViewById(R.id.division_textview)
+        Log.d(TAG, "this should be logged if the bug is fixed")
+        helloTextView.text = "Hello, debugging!"
         logging()
+        division()
     }
+    private fun division() {
+        val numerator = 60
+        var denominator = 4
+        thread(start = true) {
+            repeat(4) {
+                Thread.sleep(3000)
+                runOnUiThread {
+                    findViewById<TextView>(R.id.division_textview).setText("${numerator / denominator}")
+                    denominator--
+                }
+            }
+        }
+    }
+
     fun logging(){
-        Log.e(TAG, "ERROR:  serious errora like an app crash")
+
+        Log.e(TAG, "ERROR:  serious errors like an app crash")
         Log.w(TAG, "WARN: warns about the potential for serious errors")
         Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
         Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
